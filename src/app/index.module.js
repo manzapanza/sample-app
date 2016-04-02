7f7ecf7e42d7f0fbeb7078a7015185df64dd08eb
Massimo Rangoni <maxappbr@gmail.com>
2016-04-02 12:45:10 -0300

    feat(directives module): add directives module

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('sample', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap']);
+    .module('sample', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap', 'sample.directives']);
 
 })();
